686550a6def554a583583114c7fd39a70d893865
celder <catherine.elder@viasat.com>
2015-03-05 15:22:54 -0800

    endless scroll working

--- app/src/main/java/com/celder/gridimagesearch/activities/SearchActivity.java
+++ app/src/main/java/com/celder/gridimagesearch/activities/SearchActivity.java
@@ -11,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
 
+import com.celder.gridimagesearch.models.EndlessScrollListener;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -61,6 +62,43 @@ public class SearchActivity extends ActionBarActivity {
                 // i.putExtra("url", result); /// need to be either serializable or parseable
                 // launch the new activity
                 startActivity(i);
+            }
+        });
+        gvResults.setOnScrollListener(new EndlessScrollListener() {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount) {
+                customLoadMoreDataFromApi(page);
+            }
+        });
+    }
+
+    // Append more data into the adapter
+    public void customLoadMoreDataFromApi(int offset) {
+        // This method probably sends out a network request and appends new data items to your adapter.
+        // Use the offset value and add it as a parameter to your API request to retrieve paginated data.
+        // Deserialize API response and then construct new objects to append to the adapter
+        String query = etQuery.getText().toString();
+        // Toast.makeText(this, "Search for: " + query, Toast.LENGTH_SHORT).show();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        String searchUrl = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + query + "&rsz=8" + "&start=" + offset;
+        Log.d("DEBUG", "query triggered by scroll: " + searchUrl);
+
+        client.get(searchUrl, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // super.onSuccess(statusCode, headers, response);
+                Log.d("DEBUG", response.toString());
+                JSONArray imageResultsJson = null;
+                try {
+                    imageResultsJson = response.getJSONObject("responseData").getJSONArray("results");
+                    // changes to adapter do change underlying data
+                    imageResults.addAll(ImageResult.fromJSONArray(imageResultsJson));
+                    aImageResults.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.i("INFO", imageResults.toString());
             }
         });
     }
