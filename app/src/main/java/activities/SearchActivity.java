7342562a688cfd318edb28d6a7514dcf465f3b64
celder <catherine.elder@viasat.com>
2015-03-05 00:36:00 -0800

    image json request working

---
+++ app/src/main/java/activities/SearchActivity.java
@@ -0,0 +1,90 @@
+package activities;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.GridView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import gridimagesearch.celder.com.gridimagesearch.R;
+import models.ImageResult;
+
+
+public class SearchActivity extends ActionBarActivity {
+
+    private EditText etQuery;
+    private GridView gvResults;
+    private ArrayList<ImageResult> imageResults;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        setupViews();
+        imageResults = new ArrayList<ImageResult>();
+    }
+
+    private void setupViews() {
+        etQuery = (EditText) findViewById(R.id.etQuery);
+        gvResults = (GridView) findViewById(R.id.gvResults);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    //fired whenever button pressed
+    public void onImageSearch(View v) {
+        String query = etQuery.getText().toString();
+        // Toast.makeText(this, "Search for: " + query, Toast.LENGTH_SHORT).show();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        String searchUrl = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + query + "&rsz=8";
+        client.get(searchUrl, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // super.onSuccess(statusCode, headers, response);
+                Log.d("DEBUG", response.toString());
+                JSONArray imageResultsJson = null;
+                try {
+                    imageResultsJson = response.getJSONObject("responseData").getJSONArray("results");
+                    imageResults.clear(); // clear existing images from array (on new search)
+                    imageResults.addAll(ImageResult.fromJSONArray(imageResultsJson));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
